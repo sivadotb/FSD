e39a804f4ddb085e5b05710b4d451b8588c787a6
DESKTOP-069VPD1\Admin <balakrishnan.siva@gmail.com>
2024-03-27 19:15:55 +0530

    Text Area changes done

--- user_form_dom_script.js
+++ user_form_dom_script.js
@@ -14,16 +14,35 @@ function createInputElement(type, id, name,label){
     ele.setAttribute("name",name);
     document.body.append(ele);
 
-
-
     var break_ele2 = document.createElement("br");
     document.body.append(break_ele2);
 
 }
 
 
+function createTextAreaElement(type, id, name,label){
+   
+    var break_ele = document.createElement("br");
+    document.body.append(break_ele);
+
+    var lbl = document.createElement("label");
+    lbl.setAttribute("for",id);
+    lbl.innerHTML=label;
+    document.body.append(lbl);
+
+    var ele = document.createElement("textarea");
+    ele.setAttribute("type",type);
+    ele.setAttribute("id",id);
+    ele.setAttribute("name",name);
+    document.body.append(ele);
+
+    var break_ele2 = document.createElement("br");
+    document.body.append(break_ele2);
+
+}
+
 createInputElement("text","fname","fname", "First Name");
 createInputElement("text","mname","mname", "Middle Name");
 createInputElement("text","lname","lname", "Last Name");
 createInputElement("email","email","email", "Email");
-createInputElement("textarea","address","address", "Address");
+createTextAreaElement("textarea","address","address", "Address");
