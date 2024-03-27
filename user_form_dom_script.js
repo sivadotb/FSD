5614c06a4f0cf00ea45e89c877a0c893b60248c1
DESKTOP-069VPD1\Admin <balakrishnan.siva@gmail.com>
2024-03-27 19:39:24 +0530

    Added form

--- user_form_dom_script.js
+++ user_form_dom_script.js
@@ -1,44 +1,60 @@
-function createInputElement(type, id, name,label){
-   
-    var break_ele = document.createElement("br");
-    document.body.append(break_ele);
-
-    var lbl = document.createElement("label");
-    lbl.setAttribute("for",id);
-    lbl.innerHTML=label;
-    document.body.append(lbl);
+function createInputElement(type, id, name,label, label_flag="y", value=""){
+ 
+    if(label_flag!="n"){
+        var lbl = document.createElement("label");
+        lbl.setAttribute("for",id);
+        lbl.innerHTML=label;
+        document.getElementById("usrFrm").append(lbl);
+        createBreakElement();
+    }
 
     var ele = document.createElement("input");
     ele.setAttribute("type",type);
     ele.setAttribute("id",id);
     ele.setAttribute("name",name);
-    document.body.append(ele);
-
-    var break_ele2 = document.createElement("br");
-    document.body.append(break_ele2);
-
+    document.getElementById("usrFrm").append(ele);
+    createBreakElement();
+    if(value!=""){
+        ele.setAttribute("value",value);
+    }
 }
 
+function createBreakElement(){
+    var break_ele = document.createElement("br");
+    document.getElementById("usrFrm").append(break_ele);
+}
 
 function createTextAreaElement(type, id, name,label){
-   
-    var break_ele = document.createElement("br");
-    document.body.append(break_ele);
-
     var lbl = document.createElement("label");
     lbl.setAttribute("for",id);
     lbl.innerHTML=label;
-    document.body.append(lbl);
-
+    document.getElementById("usrFrm").append(lbl);
+    createBreakElement();
     var ele = document.createElement("textarea");
     ele.setAttribute("type",type);
     ele.setAttribute("id",id);
     ele.setAttribute("name",name);
-    document.body.append(ele);
+    document.getElementById("usrFrm").append(ele);
+    createBreakElement();
+}
 
-    var break_ele2 = document.createElement("br");
-    document.body.append(break_ele2);
+function createButtonElement(id, name,value,event,handler){
+    createBreakElement();
+    var btn = document.createElement("input");
+    btn.setAttribute("type","button");
+    btn.setAttribute("id",id);
+    btn.setAttribute("name",name);
+    btn.setAttribute("value",value);
+    btn.setAttribute(event,handler);
+    document.getElementById("usrFrm").append(btn);
+}
 
+function doSubmit(){
+    console.log(document.getElementById("fname").value);
+    console.log(document.getElementById("mname").value);
+    console.log(document.getElementById("lname").value);
+    console.log(document.getElementById("email").value);
+    console.log(document.getElementById("address").value);
 }
 
 createInputElement("text","fname","fname", "First Name");
@@ -46,3 +62,5 @@ createInputElement("text","mname","mname", "Middle Name");
 createInputElement("text","lname","lname", "Last Name");
 createInputElement("email","email","email", "Email");
 createTextAreaElement("textarea","address","address", "Address");
+createButtonElement("frmsubmit","frmsubmit", "Submit","onClick","doSubmit();");
+document.body.append(document.getElementById("usrFrm"));
