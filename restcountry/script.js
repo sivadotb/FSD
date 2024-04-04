f7ac18cf472d85ab61d3ec3d2bb579193e0cbbb3
DESKTOP-069VPD1\Admin <balakrishnan.siva@gmail.com>
2024-04-04 20:26:18 +0530

    changed the URL and design

--- restcountry/script.js
+++ restcountry/script.js
@@ -1,4 +1,4 @@
-var res = fetch("https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json");
+var res = fetch("https://restcountries.com/v3.1/all");
 res.then((data)=>data.json()).then((data1)=>(dispData(data1)));
 
 var  container = document.createElement("div");
@@ -13,17 +13,17 @@ function dispData(data){
     for(var i=0;i<data.length;i++){
         var col = document.createElement("div");
         col.className = "col-md-4";
-        col.innerHTML = `<div class="card">
-        <div class="card-header text-uppercase" >
-          ${data[i].name}
-        </div>
-        <div class="card-body">
-          <h5 class="card-title"> Capital : ${data[i].capital}</h5>
-          <p class="card-text"> Region : ${data[i].region}</p>
-          <p class="card-text"> Population : ${data[i].population}</p>
-          
-        </div>
-      </div>` ;
+
+    col.innerHTML = `<div class="card" style="width: 18rem;">
+    <img src="${data[i].flags.png}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${data[i].name.common}</h5>
+      <p class="card-text">${data[i].capital}</p>
+      <p class="card-text">${data[i].region}</p>
+      <p class="card-text">${data[i].population}</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>`
         row.append(col);
     }
 }
