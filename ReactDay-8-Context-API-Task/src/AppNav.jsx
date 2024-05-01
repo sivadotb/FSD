2afeca43f1830ed58af53528a526e29530f19eb3
sivadotb <balakrishnan.siva@gmail.com>
2024-05-01 17:41:06 +0530

    Cart state handling in progress

--- ReactDay-8-Context-API-Task/src/AppNav.jsx
+++ ReactDay-8-Context-API-Task/src/AppNav.jsx
@@ -1,33 +1,53 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import AppContext from "./AppContext";
 
 function AppNav() {
-  const storeName = "Tamil - Digital Store";
- return (
+  const { gblStoreName, cart } = useContext(AppContext);
+  return (
     <>
+      <div className="col-md-12">
         <nav className="navbar navbar-expand navbar-dark bg-success">
           <div className="container-fluid">
-            <Link className="navbar-brand" to="/">{storeName}</Link>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <Link className="navbar-brand" to="/">
+              {gblStoreName()}
+            </Link>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
               <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
               <ul className="navbar-nav">
                 <li className="nav-item">
-                  <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                  <Link className="nav-link active" aria-current="page" to="/">
+                    Home
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to="/products-list">Product Catalogue</Link>
+                  <Link className="nav-link" to="/products-list">
+                    Product Catalogue
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to="/your-cart">Cart</Link>
+                  <Link className="nav-link" to="/your-cart">
+                    Cart ({cart.length == null ? 0 : cart.length})
+                  </Link>
                 </li>
               </ul>
             </div>
           </div>
         </nav>
+      </div>
     </>
-  )
+  );
 }
-export default AppNav
+export default AppNav;
--- ReactDay-8-Context-API-Task/src/components/ProductsList.jsx
+++ ReactDay-8-Context-API-Task/src/components/ProductsList.jsx
@@ -1,18 +1,66 @@
 import AppNav from "../AppNav";
 import jsonData from "../assets/product.json";
+import { useContext } from "react";
+import AppContext from "../AppContext";
 
 function ProductsList() {
+  const { gblAppImageBasePath, setCart, cart } = useContext(AppContext);
+
+  const addToCart = function (id) {
+    //console.log(id);
+    const selectedProduct = jsonData.filter((product) => product.id == id);
+    console.log("Selected Product");
+    console.log(typeof selectedProduct);
+    console.log(selectedProduct);
+    console.log("Current Cart Product");
+    console.log(typeof cart);
+    console.log(...cart);
+    setCart([...cart, selectedProduct]);
+    console.log("after setting the cart");
+    console.log(cart);
+  };
+
   return (
     <>
       <div className="container">
-        <div>
+        <div className="row">
           <AppNav />
         </div>
-        <div>
+        <div className="row">
           {jsonData.map((product) => {
             return (
-              <div key={product.id}>
-                <p>{product.title}</p>
+              <div className="col-md-4" key={product.id}>
+                <div className="card">
+                  <img
+                    src={gblAppImageBasePath() + product.thumbnail}
+                    className="card-img-top ratio ratio-1x1"
+                    alt={product.title}
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title">{product.title}</h5>
+                    <p className="card-text">{product.description}</p>
+                  </div>
+                  <ul className="list-group list-group-flush">
+                    <li className="list-group-item">
+                      <span>Rating : </span> {product.rating}
+                    </li>
+                    <li className="list-group-item">
+                      <span>Price : </span>
+                      {product.price} $
+                    </li>
+                  </ul>
+                  <div className="card-body">
+                    <a
+                      href="#"
+                      className="card-link"
+                      onClick={() => {
+                        addToCart(product.id);
+                      }}
+                    >
+                      Add To Cart
+                    </a>
+                  </div>
+                </div>
               </div>
             );
           })}
